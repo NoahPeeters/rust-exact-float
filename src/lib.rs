5a10a43505228fdb850880c1b7a07d0b4fd1bd4d
Noah Peeters <noah.peeters@icloud.com>
2016-06-04 00:11:29 +0200

    Basic ExactFloat struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+
+#![allow(dead_code)]
+struct ExactFloat {
+    sign: bool,
+    numerator: u64,
+    denominator: u64
+}
+
+impl ExactFloat {
+    pub fn from_integer(x: i64) -> ExactFloat {
+        ExactFloat {
+            sign: x < 0,
+            numerator: x.abs() as u64,
+            denominator: 1
+        }
     }
+    pub fn calculate(&self) -> f64 {
+        self.numerator as f64 / self.denominator as f64
+    }
+}
+
+#[test]
+fn it_works() {
+     assert_eq!(ExactFloat::from_integer(10).calculate(), 10.0);
 }
